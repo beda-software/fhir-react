618a666cfb321903ef899f03a7cf50a5187f1a0e
Dmitry Shutov <dmitry.s@beda.software>
2023-08-24 17:27:29 +0700

    Return response status from fetch service

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,14 +1,26 @@
-import { RemoteDataResult, failure, success } from "../libs/remoteData"
+import { RemoteDataResult, failure, success } from '../libs/remoteData';
 
-export async function service<S = any, F = any>(request: RequestInfo | URL, init?: RequestInit): Promise<RemoteDataResult<S, F>> {
+export interface FetchError {
+    message: string;
+    status?: number;
+}
+
+export async function service<S = any>(
+    request: RequestInfo | URL,
+    init?: RequestInit
+): Promise<RemoteDataResult<S, FetchError>> {
     try {
-        const response = await fetch(request, init)
+        const response = await fetch(request, init);
         if (response.ok) {
-            return success(await response.json())
+            return success(await response.json());
         } else {
-            return failure(await response.text() as any)
+            return failure<FetchError>({ message: await response.text(), status: response.status });
         }
-    } catch (err: any) {
-        return failure(err.message);
+    } catch (err) {
+        if (err instanceof Error) {
+            return failure({ message: err.message });
+        }
+
+        return failure({ message: 'Unknown error' });
     }
 }
